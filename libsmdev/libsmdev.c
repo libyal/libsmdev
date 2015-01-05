5d112559291e78208f169450d8cfa7ee09d439b6
Joachim Metz <joachim.metz@gmail.com>
2015-01-05 18:09:42 +0100

    Applied updates.

--- libsmdev/libsmdev.c
+++ libsmdev/libsmdev.c
@@ -1,7 +1,7 @@
 /*
  * Library to access and read storage media (SM) devices
  *
- * Copyright (c) 2010-2014, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2015, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
--- pysmdev/pysmdev_handle.c
+++ pysmdev/pysmdev_handle.c
@@ -1,8 +1,7 @@
 /*
  * Python object definition of the libsmdev handle
  *
- * Copyright (c) 2008, David Collett <david.collett@gmail.com>
- * Copyright (c) 2010-2014, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2015, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
