9f448c9cad7902321f97382c50415969d5ae60ff
Joachim Metz <joachim.metz@gmail.com>
2018-04-03 08:59:36 +0200

    Applied updates

--- tests/smdev_test_notify.c
+++ tests/smdev_test_notify.c
@@ -27,6 +27,7 @@
 #include <stdlib.h>
 #endif
 
+#include "smdev_test_libcerror.h"
 #include "smdev_test_libsmdev.h"
 #include "smdev_test_macros.h"
 #include "smdev_test_unused.h"
@@ -51,13 +52,37 @@ int smdev_test_notify_set_verbose(
 int smdev_test_notify_set_stream(
      void )
 {
-	/* Test invocation of function only
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test regular cases
 	 */
-	libsmdev_notify_set_stream(
-	 NULL,
-	 NULL );
+	result = libsmdev_notify_set_stream(
+	          NULL,
+	          &error );
+
+	SMDEV_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	SMDEV_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+/* TODO test libcnotify_stream_set failure */
 
 	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
 }
 
 /* Tests the libsmdev_notify_stream_open function
@@ -66,13 +91,65 @@ int smdev_test_notify_set_stream(
 int smdev_test_notify_stream_open(
      void )
 {
-	/* Test invocation of function only
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test regular cases
 	 */
-	libsmdev_notify_stream_open(
-	 NULL,
-	 NULL );
+	result = libsmdev_notify_stream_open(
+	          "notify_stream.log",
+	          &error );
+
+	SMDEV_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 1 );
+
+	SMDEV_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+	result = libsmdev_notify_stream_open(
+	          NULL,
+	          &error );
+
+	SMDEV_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	SMDEV_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	/* Clean up
+	 */
+	result = libsmdev_notify_stream_close(
+	          &error );
+
+	SMDEV_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 0 );
+
+	SMDEV_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
 
 	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
 }
 
 /* Tests the libsmdev_notify_stream_close function
@@ -81,12 +158,36 @@ int smdev_test_notify_stream_open(
 int smdev_test_notify_stream_close(
      void )
 {
-	/* Test invocation of function only
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test regular cases
 	 */
-	libsmdev_notify_stream_close(
-	 NULL );
+	result = libsmdev_notify_stream_close(
+	          &error );
+
+	SMDEV_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 0 );
+
+	SMDEV_TEST_ASSERT_IS_NULL(
+	 "error",
+	 error );
+
+	/* Test error cases
+	 */
+/* TODO test libcnotify_stream_close failure */
 
 	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
 }
 
 /* The main program
