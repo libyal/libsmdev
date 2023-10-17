9d5f54a021d686270983ef2af2c3d5cd9e6d67d7
Joachim Metz <joachim.metz@gmail.com>
2023-10-17 06:42:02 +0200

    Changes for CI tests

--- tests/test_smdevinfo.sh
+++ tests/test_smdevinfo.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Info tool testing script
 #
-# Version: 20230410
+# Version: 20231005
 
 EXIT_SUCCESS=0;
 EXIT_FAILURE=1;
@@ -9,7 +9,7 @@ EXIT_IGNORE=77;
 
 PROFILES=("smdevinfo");
 OPTIONS_PER_PROFILE=("");
-OPTION_SETS="";
+OPTION_SETS=();
 
 INPUT_GLOB="*";
 
@@ -32,11 +32,6 @@ then
 	exit ${EXIT_FAILURE};
 fi
 
-if test "${OSTYPE}" = "msys";
-then
-	exit ${EXIT_IGNORE};
-fi
-
 TEST_RUNNER="tests/test_runner.sh";
 
 if ! test -f "${TEST_RUNNER}";
@@ -53,20 +48,102 @@ fi
 
 source ${TEST_RUNNER};
 
-INPUT_FILE="/dev/sda";
-
-if ! test -b ${INPUT_FILE} || ! test -r ${INPUT_FILE};
+if ! test -d "input";
 then
-	INPUT_FILE="/dev/vda";
+	echo "Test input directory not found.";
+
+	exit ${EXIT_IGNORE};
+fi
+RESULT=`ls input/* | tr ' ' '\n' | wc -l`;
+
+if test ${RESULT} -eq ${EXIT_SUCCESS};
+then
+	echo "No files or directories found in the test input directory";
+
+	exit ${EXIT_IGNORE};
 fi
 
-if ! test -b ${INPUT_FILE} || ! test -r ${INPUT_FILE};
-then
-	INPUT_FILE="/dev/null";
-fi
+for PROFILE_INDEX in ${!PROFILES[*]};
+do
+	TEST_PROFILE=${PROFILES[${PROFILE_INDEX}]};
 
-run_test_on_input_file "smdevinfo" "smdevinfo" "with_stdout_reference" "${OPTION_SETS}" "${TEST_EXECUTABLE}" "${INPUT_FILE}" "${OPTIONS}";
-RESULT=$?;
+	TEST_PROFILE_DIRECTORY=$(get_test_profile_directory "input" "${TEST_PROFILE}");
+
+	IGNORE_LIST=$(read_ignore_list "${TEST_PROFILE_DIRECTORY}");
+
+	IFS=" " read -a PROFILE_OPTIONS <<< ${OPTIONS_PER_PROFILE[${PROFILE_INDEX}]};
+
+	RESULT=${EXIT_SUCCESS};
+
+	for TEST_SET_INPUT_DIRECTORY in input/*;
+	do
+		if ! test -d "${TEST_SET_INPUT_DIRECTORY}";
+		then
+			continue;
+		fi
+		TEST_SET=`basename ${TEST_SET_INPUT_DIRECTORY}`;
+
+		if check_for_test_set_in_ignore_list "${TEST_SET}" "${IGNORE_LIST}";
+		then
+			continue;
+		fi
+		TEST_SET_DIRECTORY=$(get_test_set_directory "${TEST_PROFILE_DIRECTORY}" "${TEST_SET_INPUT_DIRECTORY}");
+
+		RESULT=${EXIT_SUCCESS};
+
+		if test -f "${TEST_SET_DIRECTORY}/files";
+		then
+			IFS="" read -a INPUT_FILES <<< $(cat ${TEST_SET_DIRECTORY}/files | sed "s?^?${TEST_SET_INPUT_DIRECTORY}/?");
+		else
+			IFS="" read -a INPUT_FILES <<< $(ls -1d ${TEST_SET_INPUT_DIRECTORY}/${INPUT_GLOB});
+		fi
+		for INPUT_FILE in "${INPUT_FILES[@]}";
+		do
+			TESTED_WITH_OPTIONS=0;
+
+			for OPTION_SET in ${OPTION_SETS[@]};
+			do
+				TEST_DATA_OPTION_FILE=$(get_test_data_option_file "${TEST_SET_DIRECTORY}" "${INPUT_FILE}" "${OPTION_SET}");
+
+				if test -f ${TEST_DATA_OPTION_FILE};
+				then
+					TESTED_WITH_OPTIONS=1;
+
+					IFS=" " read -a OPTIONS <<< $(read_test_data_option_file "${TEST_SET_DIRECTORY}" "${INPUT_FILE}" "${OPTION_SET}");
+
+					run_test_on_input_file "${TEST_SET_DIRECTORY}" "smdevinfo" "with_stdout_reference" "${OPTION_SET}" "${TEST_EXECUTABLE}" "${INPUT_FILE}" "${PROFILE_OPTIONS[@]}" "${OPTIONS[@]}";
+					RESULT=$?;
+
+					if test ${RESULT} -ne ${EXIT_SUCCESS};
+					then
+						break;
+					fi
+				fi
+			done
+
+			if test ${TESTED_WITH_OPTIONS} -eq 0;
+			then
+				run_test_on_input_file "${TEST_SET_DIRECTORY}" "smdevinfo" "with_stdout_reference" "" "${TEST_EXECUTABLE}" "${INPUT_FILE}" "${PROFILE_OPTIONS[@]}";
+				RESULT=$?;
+			fi
+
+			if test ${RESULT} -ne ${EXIT_SUCCESS};
+			then
+				break;
+			fi
+		done
+
+		# Ignore failures due to corrupted data.
+		if test "${TEST_SET}" = "corrupted";
+		then
+			RESULT=${EXIT_SUCCESS};
+		fi
+		if test ${RESULT} -ne ${EXIT_SUCCESS};
+		then
+			break;
+		fi
+	done
+done
 
 exit ${RESULT};
 
