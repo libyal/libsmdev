191fd67bc1626659443e05cadd180163f9ccc7fe
Joachim Metz <joachim.metz@gmail.com>
2026-09-17 04:35:51 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libsmdev/.libs/libsmdev.1.dylib && test -f ./pysmdev/.libs/pysmdev.so
+if [ -f "${PWD}/libsmdev/.libs/libsmdev.1.dylib" ] && [ -f ./pysmdev/.libs/pysmdev.so ]
 then
-	install_name_tool -change /usr/local/lib/libsmdev.1.dylib ${PWD}/libsmdev/.libs/libsmdev.1.dylib ./pysmdev/.libs/pysmdev.so
+    install_name_tool -change /usr/local/lib/libsmdev.1.dylib "${PWD}/libsmdev/.libs/libsmdev.1.dylib" ./pysmdev/.libs/pysmdev.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
